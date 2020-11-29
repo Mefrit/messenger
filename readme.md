433a6d9d05b5769110a5331bbfe66913779b6fb0
Mefrit <mihail_fokin_1999@mail.ru>
2020-11-30 01:10:54 +0300

    get message from server

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # start server
 pip install websockets
-
+// пришлось отказаться от web-soket тк трудности с выкладыванием на хостинг + у нас не будет чата?
 python server.py
--- server.py
+++ server.py
@@ -1,49 +1,105 @@
+# python server.py
 
+from http.server import BaseHTTPRequestHandler, HTTPServer,CGIHTTPRequestHandler # python3
+import json
+class HandleRequests(BaseHTTPRequestHandler):
+    def _set_headers_html(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _set_headers_json(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        result = dict()
+        print("self.path!!!!!!!!!!!!!!!!!!!!!!!",self.path, self.path.endswith('/?action=test'))
+        if self.path.endswith('/?action=test'):
+            print("JSSSSSSSSSSSSSSSSSSSSSSOOOOOOOOOOOOOOOOOOOOOOONnnnnnnnnnnnnnnnnnnn")
+            self._set_headers_json()
+            result["answer"] = self.path + "json"
+            result = json.dumps(result)
+            self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}).encode())
+            return
+        else:
+            self._set_headers_html()
+            #  self.path - текущий путь в url
+            print("FILEOPEN !!!!@@#!@#!@@@@@@@@@@@@@",self.path)
+            f = open("." + self.path, 'rb')
+            # self.wfile.write(result)
+            self.wfile.write(f.read())
+            f.close()
+            return
+            # result["answer"] = self.path+ "html"
+            # result = json.dumps(result) 
+            # self.wfile.write(result.encode())
+       
+
+        # return result.encode()
+
+    def do_POST(self):
+        '''Reads post request body'''
+        self._set_headers_html()
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        self.wfile.write("received post request:<br>{}".format(post_body))
+
+    def do_PUT(self):
+        self.do_POST()
+
+def main():
+    PORT = 8000
+    server = HTTPServer(('',PORT), HandleRequests)
+    print("Server runing in port %s" % PORT)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
 
 # from http.server import HTTPServer, CGIHTTPRequestHandler
 # server_address = ("", 8002)
 # httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 # httpd.serve_forever()
+
+
 # from http.server import HTTPServer, BaseHTTPRequestHandler
+# import socket
+
+# sock = socket.socket()
+# sock.bind(('', 9090))
+# sock.listen(1)
+# conn, addr = sock.accept()
+
+# print ('connected:', addr)
+
+# while True:
+#     data = conn.recv(1024)
+#     if not data:
+#         break
+#     conn.send(data.upper())
 
 
-# class Serv(BaseHTTPRequestHandler):
 
-#     def do_GET(self):
-       
-#         if self.path == '/':
-#             self.path = './public/index.html'
-#         if self.path == '/public':
-#             self.path = '/index.html'
-#         try:
-            
-#             file_to_open = open(self.path[1:]).read()
-#             self.send_response(200)
-#         except:
-#             file_to_open = "File not found11"
-#             self.send_response(404)
-#         self.end_headers()
-#         self.wfile.write(bytes(file_to_open, 'utf-8'))
+# conn.close()
 
+# work
+# import asyncio
+# import datetime
+# import random
+# import websockets
 
-# httpd = HTTPServer(('localhost', 8080), Serv)
-# httpd.serve_forever()
+# async def time(websocket, path):
+#     while True:
+#         now = datetime.datetime.utcnow().isoformat() + 'Z'
+#         await websocket.send(now)
+#         await asyncio.sleep(random.random() * 3)
 
-import asyncio
-import datetime
-import random
-import websockets
+# start_server = websockets.serve(time, '127.0.0.1', 5678)
 
-async def time(websocket, path):
-    while True:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        await websocket.send(now)
-        await asyncio.sleep(random.random() * 3)
-
-start_server = websockets.serve(time, '127.0.0.1', 5678)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()
 
 
 
